a07e1d17fa2e8a93d30e230bb68c16de38f27ad8
Pedro Acosta P <pcost8300@me.com>
2018-05-14 12:41:25 -0500

    Commit Mejora: Se agregó una funcion para detectar si un item de un DATALIST ha sido seleccionado

--- VERSION.md
+++ VERSION.md
@@ -1,5 +1,6 @@
 ### v1.1.0
 * Se agrega un modulo para hacer peticiones ajax.
+* Se agrega funcion para Alertar si un dato de un DATALIST ha sido seleccionado.
 
 ### v1.0.1
 * Se elimino una funcion que no resulta efectiva en ningun modo.
--- domManager/jsCheckers.js
+++ domManager/jsCheckers.js
@@ -4,11 +4,25 @@
  * 
  * @function inputIsEmpty Devuelve verdadero si el input no contiene texto onkeyup.
  * @param {DOM} c recibe el DOM para poder recabar los datos
- *  
+ * 
+ * @function listItemSelected Reacciona cuando un elemento de un datalist ha sido seleccionado
+ * @param {DOM} c recibe el DOM para poder ubicar la lista especifica 
+ * 
  */
 
 console.log('- Checkers Added');
 
 const emptyTarget = (tar) => {
     return typeof tar == typeof undefined || tar === false; 
+};
+
+const listItemSelected = (c) => {
+    let inputVal = c.value;
+    let listData = document.getElementById($(c).attr('list')).childNodes;
+    for (var i = 0; i < listData.length; i++){
+        if (listData[i].value === val){
+            return true;
+            break;
+        }
+    }
 };
